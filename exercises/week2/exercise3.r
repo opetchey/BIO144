8b23633ac90f35c8c730a2fdbdb5daefde1f5362
Owen Petchey <owen.petchey@ieu.uzh.ch>
2017-01-17 16:55:45 +0100

    update exercise 3 week2

--- exercises/week2/exercise3.r
+++ exercises/week2/exercise3.r
@@ -1,5 +1,34 @@
 rm(list=ls())
 
+library(readr)
 library(tidyverse)
 
-read.csv()
+fhc <- read_csv("https://raw.githubusercontent.com/opetchey/BIO144/master/datasets/financing_healthcare.csv")
+
+unique(fhc$country)
+unique(fhc$year)
+
+filter(fhc, !is.na(child_mort) & !is.na(health_exp_total))
+
+fhc1 <- financing_healthcare %>%
+  filter(year==2013) %>%
+  select(year:continent, health_exp_total, child_mort, life_expectancy) %>% 
+  drop_na() %>% 
+  group_by(country)
+
+group_by(fhc1, continent) %>%
+  summarise(mean=mean(child_mort),
+            sd=sd(child_mort))
+
+qplot(x=continent, y=child_mort, data=fhc1, geom="boxplot")
+
+qplot(x=health_exp_total, y=child_mort, colour=continent, data=fhc2) 
+qplot(x=health_exp_total, y=child_mort, colour=continent, data=fhc2) + geom_label(aes(label=country))
+
+qplot(x=health_exp_total, y=life_expectancy, colour=continent, data=fhc2) 
+qplot(x=health_exp_total, y=child_mort, colour=continent, data=fhc2) + geom_label(aes(label=country))
+
+
+qplot(x=log10(health_exp_total), y=log10(child_mort), data=fhc2)
+
+
