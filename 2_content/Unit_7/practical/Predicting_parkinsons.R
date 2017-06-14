6a1315a57536af08b55172e1f473a4c59abbcb87
Owen Petchey <owen.petchey@ieu.uzh.ch>
2017-06-14 19:46:13 +0200

    various

--- 2_content/Unit_7/practical/Predicting_parkinsons.R
+++
@@ -1,8 +0,0 @@
-rm(list=ls())
-
-dd <- read_csv("~/Desktop/parkinsons_updrs_data.csv")
-
-ggplot(dd, aes(x=total_UPDRS)) + geom_density(bw=5)
-ggplot(dd, aes(x=total_UPDRS)) + geom_density(bw=2)
-ggplot(dd, aes(x=total_UPDRS)) + geom_density(bw=0.5)
-ggplot(dd, aes(x=total_UPDRS)) + geom_density(bw=0.05)
