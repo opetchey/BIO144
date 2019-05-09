9d5121f3aeeee1c34fd457b9d3c008115d84a7cb
Owen <owen.petchey@ieu.uzh.ch>
2019-05-09 20:37:22 +0200

    Update abalone_age_glm.R

--- 2_content/Unit_10/practical/abalone_age_glm.R
+++ 2_content/Unit_10/practical/abalone_age_glm.R
@@ -23,35 +23,36 @@ ggplot(dd, aes(x=Rings)) + geom_histogram(bins=15)
 full_mod <- glm(Rings ~ Sex + Length_mm + Diameter_mm + Height_mm + Whole_weight_g +
                  Shuck_weight_g + Viscera_weight_g + Shell_weight_g, data=dd,
                 family=poisson)
-summary(full_mod)
-## remove length_mm
-m1 <- glm(Rings ~ Sex + Diameter_mm + Height_mm + Whole_weight_g +
-                 Shuck_weight_g + Viscera_weight_g + Shell_weight_g, data=dd,
-          family=poisson)
-summary(m1)
-## remove shell weight
-m2 <- glm(Rings ~ Sex + Diameter_mm + Height_mm + Whole_weight_g +
-           Shuck_weight_g + Viscera_weight_g, data=dd,
-          family=poisson)
-summary(m2)
-## remove height
-m3 <- glm(Rings ~ Sex + Diameter_mm + Whole_weight_g +
-           Shuck_weight_g + Viscera_weight_g, data=dd,
-          family=poisson)
-summary(m3)
-## at this point in the lm we stopped. All were significant at 0.05
-## Viscera_weight_g -14.5064     4.1044  -3.534 0.000457 ***
 
-m4 <- glm(Rings ~ Sex + Diameter_mm + Whole_weight_g +
-            Shuck_weight_g, data=dd,
-          family=poisson)
-summary(m4)
+dropterm(full_mod, sorted=TRUE)
+m1 <- update(full_mod, . ~ . - Length_mm)
+AICc(m1)
 
-m5 <- glm(Rings ~ Diameter_mm + Whole_weight_g +
-            Shuck_weight_g, data=dd,
-          family=poisson)
-summary(m5)
-autoplot(m5)
+dropterm(m1, sorted=TRUE)
+m2 <- update(m1, . ~ . - Shell_weight_g)
+AICc(m2)
+
+dropterm(m2, sorted=TRUE)
+m3 <- update(m2, . ~ . - Height_mm)
+AICc(m3)
+
+dropterm(m3, sorted=TRUE)
+m4 <- update(m3, . ~ . - Sex)
+AIC(m4)
+
+dropterm(m4, sorted=TRUE)
+m5 <- update(m4, . ~ . - Sex)
+AIC(m5)
+
+dropterm(m5, sorted=TRUE)
+m6 <- update(m5, . ~ . - Viscera_weight_g)
+AIC(m6)
+
+dropterm(m6, sorted=TRUE)
+
+
+summary(m6)
+autoplot(m6)
 
 ## for next line to work, need to do linear m3
 lm3 <- lm(Rings ~ Sex + Diameter_mm + Whole_weight_g +
@@ -63,49 +64,6 @@ ggplot(mapping=aes(x=fitted(m5), y=fitted(lm3))) + geom_point() +
 cor(cbind(x=fitted(m5), y=fitted(m3)))
 
 
-## qqplot not great, and some increase in residuals with fitted...
-## actually, we could have anticipated this, for a particular reason that
-## we'll discuss later in the course.
-## Then we'll attempt to correct the situation.
-
-
-mods <- list(full_mod=full_mod, m1=m1, m2=m2, m3=m3, m4=m4, m5=m5)
-aictab(mods)
-
-
-
-## Use step AIC
-m0 <- glm(Rings ~ 1, data=dd, family=poisson)
-s1 <- stepAIC(full_mod, direction = "backward", AICc=TRUE)
-
-s2 <- stepAIC(m0, direction = "forward", AICc=TRUE,
-              scope=list(lower=m0, upper=full_mod))
-
-s3 <- stepAIC(m0, direction = "both", AICc=TRUE,
-              scope=list(lower=m0, upper=m1))
-
-aictab(list(m3=m3, s1=s1, s2=s2, s3=s3))
-s1
-
-## package glmulti model selection:: the death star!
-
-multi1 <- glmulti(Rings ~ Sex + Length_mm + Diameter_mm + Height_mm + Whole_weight_g +
-                    Shuck_weight_g + Viscera_weight_g + Shell_weight_g, data=dd,
-                  level = 1,
-                  method = "h",
-                  crit = "aicc",
-                  confsetsize = 10,
-                  plotty=F, report=F,
-                  fitfunction = "glm", family=poisson)
-
-print(multi1)
-plot(multi1, type="s")
-plot(multi1, type="p")
-plot(multi1, type="w")
-
-coef(multi1)
-
-predict(multi1)
 
 
 
