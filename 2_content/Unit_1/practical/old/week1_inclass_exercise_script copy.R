585f23fba53c571b520805fe7d44623407b26b5c
Owen Petchey <owen.petchey@ieu.uzh.ch>
2025-02-15 08:43:14 +0100

    update exercise 1

--- 2_content/Unit_1/practical/old/week1_inclass_exercise_script copy.R
+++ 2_content/Unit_1/practical/old/week1_inclass_exercise_script copy.R
@@ -1,4 +1,5 @@
-## This is the solution script for the first practical
+## Warning: the code below contains deliberate errors.
+## Also it sometimes contains "???" which you need to replace with appropriate text.
 
 
 #######################################################
@@ -21,19 +22,19 @@ library(skimr)
 ## Now read in the data, using the read_csv() function.
 ## First we should assign, using the assignment arrow,
 ## the URL of the published version of the google sheet data into an object.
-the_URL <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vTl3AnTRihwmiBfyKukIecYH9qMI507JFriFszHLHO8c6WfLfIQx0nnWIHkMXQO-vvcWkg5Y-u17JkR/pub?gid=1774295787&single=true&output=csv"
+the_URL - "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0S581py-IDp4bIQnqFFTttQ1JChFHMMjgkYEbSv88nD1yqV1ocNY1eqrLifEPOHxkCZ4q73XeJcTX/pub?gid=1441390006&single=true&output=csv"
 ## then use the read_csv function to read in the data from that URL
-class_RTs <- read_csv(the_URL)
+class_RTs <- read_cvs(the_URL)
 #######################################################
 
 ## DO NOT USE read.csv above!!!
 
-## --->>> Once more, and even if a TA tells you to, do not use read.csv on line 25.
+## --->>> Once more, and even if a TA tells you to, do not use read.csv on line 27.
 ## --->>> You must keep the underscore (_) and not replace it with a dot (.)
 
 #######################################################
 ## Have a look at the data in R, does it look OK?
-class_RTs
+clas_RTs
 #######################################################
 
 
@@ -42,27 +43,19 @@ class_RTs
 ## Clean up the column / variable names:
 ## Must be very careful to get the next line right!!! Really important!!!
 ## Otherwise columns will have the wrong names, which would be very confusing
-names(class_RTs) <- c("Timestamp",
-                      "Gender",
-                      "Weight",
-                      "Handedness",
-                      "Pref_Reaction_time_1",
-                      "Pref_Reaction_time_2",
-                      "Pref_Reaction_time_3", 
-                      "Pref_Reaction_time_4",
-                      "Pref_Reaction_time_5",
+names(class_RTs) <- c("Timestamp", "ID", "Gender", "Weight"
+                      "Handedness", "Pref_Reaction_time_1",
+                      "Pref_Reaction_time_2", "Pref_Reaction_time_3", 
+                      "Pref_Reaction_time_4", "Pref_Reaction_time_5",
                       "Pref_Reaction_time",
                       "Nonpref_Reaction_time_ave",
-                      "Verbal_memory_score",
-                      "Number_memory_score",
-                      "Visual_memory_score",
-                      "Random_number")
+                      "Verbal_memory_score", "Number_memory_score",
+                      "Visual_memory_score", "Random_number")
 ## check the variable names are what we just tried to set them to be
-names(class_RTs)
+class_RTs
 #######################################################
 
-## View the data
-View(class_RTs)
+
 
 #######################################################
 ## Check the variable types are correct
@@ -72,7 +65,7 @@ View(class_RTs)
 ## Gender should be a character
 ## Handed should be character
 ## The remaining variables should be numeric (<dbl>)
-str(class_RTs)
+glimpse(class_RTs
 #######################################################
 
 
@@ -80,46 +73,46 @@ str(class_RTs)
 ## Correct or exclude problematic data
 ## If we have problems here, with variables of the wrong type,
 ## it probably means some of the data entry is a bit messed up.
-## the skim() function is a one for looking at the data,
+## the skim() function is a really nice one for looking at the data,
 ## including if any variables have missing values (NAs)
-skim(class_RTs)
+skim(class_RT)
 
 
 #######################################################
 ## and the number of observations of each gender
 class_RTs %>%
-  group_by(Gender) %>%
+  group_by(Gender) %%
   summarise(number = n())
 #######################################################
 
 
 #######################################################
 ## Now make a figure containing the histogram of reaction times
-ggplot(data=class_RTs, aes(x=Pref_Reaction_time)) +
+ggplot(data=class_RTs, aes(x=???)) +
   geom_histogram()
 
 ## Now make a figure containing two histograms histograms (i.e. two "facets"), one for each gender
-ggplot(data=class_RTs, aes(x=Pref_Reaction_time)) +
+ggplot(data=class_RTs, aes(x=???)) +
   geom_histogram() +
-  facet_grid(~ Gender)
+  facet_grid(~Gender)
 
 ## And a box and whisker plot
-ggplot(data=class_RTs, aes(x=Gender, y=Pref_Reaction_time)) +
+ggplot(data=class_RTs, aes(x=???, y=???)) +
   geom_boxplot()
 
 ## Or just the data points (with some jitter, to separate overlapping points):
-ggplot(data=class_RTs, aes(x=Gender, y=Pref_Reaction_time)) +
+ggplot(data=class_RTs, aes(x=???, y=???)) +
   geom_jitter(width=0.05)
 #######################################################
 
 #######################################################
 ## Perhaps filter out some extreme values
 class_RTs_filtered <- class_RTs %>%
-  filter(Pref_Reaction_time > 50,
-         Pref_Reaction_time < 500)
-class_RTs_filtered %>%
-ggplot() +
-  geom_jitter(mapping = aes(x=Gender, y=Pref_Reaction_time),
+  filter(Pref_Reaction_time < 50,
+         Pref_Reaction_time > 500)
+class_RTs_filtred %>%
+  ggplot2() +
+  geom_jitter(mapping = aes(x=Gnder, y=Pref_reaction_time),
               width=0.05)
 #######################################################
 
@@ -137,20 +130,20 @@ ggplot() +
 
 
 #######################################################
-## Do a t-test and assign the outcome to an object:
-my_ttest <- t.test(Pref_Reaction_time ~ Gender,
+## Do the t test and assign the outcome to an object:
+my_ttest <- t.test(??? ~ ???,
                    data=class_RTs,
                    var.equal=TRUE)
 ## look at the result of the t-test
-my_ttest
+t.test
 #######################################################
 
 ###################
 ## And the same but with the filtered data:
-my_ttest <- t.test(Pref_Reaction_time ~ Gender,
-                   data = class_RTs_filtered,
+my_ttest <- t.test(??? ~ ???,
+                   data = ???,
                    var.equal=TRUE)
-my_ttest
+t.test
 
 
 
@@ -171,7 +164,6 @@ my_ttest
 ## Write a sentence that gives the direction and extent of difference,
 ## and a measure of certainty / uncertainty in that finding.
 ## Make a beautiful graph that very clearly communicates the findings!
-ggplot(data=class_RTs_filtered, aes(x=Gender, y=Pref_Reaction_time)) +
+ggplot(data=???, aes(x=???, y=???)) +
   geom_boxplot() +
-  geom_jitter(width= 0.1) +
-  ylab("Reaction time (milliseconds)")
+  ylab("Reaction time (seconds)")
--- 2_content/Unit_1/practical/week1_inclass_exercise_script.R
+++ 2_content/Unit_1/practical/week1_inclass_exercise_script.R
@@ -1,5 +1,4 @@
-## Warning: the code below contains deliberate errors.
-## Also it sometimes contains "???" which you need to replace with appropriate text.
+## This is the solution script for the first practical
 
 
 #######################################################
@@ -22,14 +21,14 @@ library(skimr)
 ## Now read in the data, using the read_csv() function.
 ## First we should assign, using the assignment arrow,
 ## the URL of the published version of the google sheet data into an object.
-the_URL - "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0S581py-IDp4bIQnqFFTttQ1JChFHMMjgkYEbSv88nD1yqV1ocNY1eqrLifEPOHxkCZ4q73XeJcTX/pub?gid=1441390006&single=true&output=csv"
+the_URL - "https://docs.google.com/spreadsheets/d/e/2PACX-1vTl3AnTRihwmiBfyKukIecYH9qMI507JFriFszHLHO8c6WfLfIQx0nnWIHkMXQO-vvcWkg5Y-u17JkR/pub?gid=1774295787&single=true&output=csv"
 ## then use the read_csv function to read in the data from that URL
 class_RTs <- read_cvs(the_URL)
 #######################################################
 
 ## DO NOT USE read.csv above!!!
 
-## --->>> Once more, and even if a TA tells you to, do not use read.csv on line 27.
+## --->>> Once more, and even if a TA tells you to, do not use read.csv on line 25.
 ## --->>> You must keep the underscore (_) and not replace it with a dot (.)
 
 #######################################################
@@ -43,19 +42,27 @@ clas_RTs
 ## Clean up the column / variable names:
 ## Must be very careful to get the next line right!!! Really important!!!
 ## Otherwise columns will have the wrong names, which would be very confusing
-names(class_RTs) <- c("Timestamp", "ID", "Gender", "Weight"
-                      "Handedness", "Pref_Reaction_time_1",
-                      "Pref_Reaction_time_2", "Pref_Reaction_time_3", 
-                      "Pref_Reaction_time_4", "Pref_Reaction_time_5",
+names(class_RTs) <- c("Timestamp",
+                      "Gender",
+                      "Weight"
+                      "Handedness",
+                      "Pref_Reaction_time_1",
+                      "Pref_Reaction_time_2",
+                      "Pref_Reaction_time_3", 
+                      "Pref_Reaction_time_4",
+                      "Pref_Reaction_time_5",
                       "Pref_Reaction_time",
                       "Nonpref_Reaction_time_ave",
-                      "Verbal_memory_score", "Number_memory_score",
-                      "Visual_memory_score", "Random_number")
+                      "Verbal_memory_score",
+                      "Number_memory_score",
+                      "Visual_memory_score",
+                      "Random_number")
 ## check the variable names are what we just tried to set them to be
-class_RTs
+names(class_RT)
 #######################################################
 
-
+## View the data
+View(class_RTs)
 
 #######################################################
 ## Check the variable types are correct
@@ -65,7 +72,7 @@ class_RTs
 ## Gender should be a character
 ## Handed should be character
 ## The remaining variables should be numeric (<dbl>)
-glimpse(class_RTs
+str(Class_RTs)
 #######################################################
 
 
@@ -73,15 +80,15 @@ glimpse(class_RTs
 ## Correct or exclude problematic data
 ## If we have problems here, with variables of the wrong type,
 ## it probably means some of the data entry is a bit messed up.
-## the skim() function is a really nice one for looking at the data,
+## the skim() function is a one for looking at the data,
 ## including if any variables have missing values (NAs)
-skim(class_RT)
-
+skim(class_RTs)
+## Lots of information. Work through it, and ask for help if you need it.
 
 #######################################################
 ## and the number of observations of each gender
 class_RTs %>%
-  group_by(Gender) %%
+  group_by(Gender) %>%
   summarise(number = n())
 #######################################################
 
@@ -94,25 +101,25 @@ ggplot(data=class_RTs, aes(x=???)) +
 ## Now make a figure containing two histograms histograms (i.e. two "facets"), one for each gender
 ggplot(data=class_RTs, aes(x=???)) +
   geom_histogram() +
-  facet_grid(~Gender)
+  facet_grid(~ Gender)
 
 ## And a box and whisker plot
 ggplot(data=class_RTs, aes(x=???, y=???)) +
   geom_boxplot()
 
 ## Or just the data points (with some jitter, to separate overlapping points):
-ggplot(data=class_RTs, aes(x=???, y=???)) +
+ggplot(data=class_RTs, aes(x=Gender, y=Pref_Reaction_time)) +
   geom_jitter(width=0.05)
 #######################################################
 
 #######################################################
 ## Perhaps filter out some extreme values
 class_RTs_filtered <- class_RTs %>%
-  filter(Pref_Reaction_time < 50,
-         Pref_Reaction_time > 500)
-class_RTs_filtred %>%
-  ggplot2() +
-  geom_jitter(mapping = aes(x=Gnder, y=Pref_reaction_time),
+  filter(Pref_Reaction_time > 50,
+         Pref_Reaction_time < 500)
+class_RTs_filtered %>%
+ggplot() +
+  geom_jitter(mapping = aes(x=Gnder, y=Pref_Reaction_time),
               width=0.05)
 #######################################################
 
@@ -130,7 +137,7 @@ class_RTs_filtred %>%
 
 
 #######################################################
-## Do the t test and assign the outcome to an object:
+## Do a t-test and assign the outcome to an object:
 my_ttest <- t.test(??? ~ ???,
                    data=class_RTs,
                    var.equal=TRUE)
@@ -143,7 +150,7 @@ t.test
 my_ttest <- t.test(??? ~ ???,
                    data = ???,
                    var.equal=TRUE)
-t.test
+my_ttest
 
 
 
@@ -166,4 +173,5 @@ t.test
 ## Make a beautiful graph that very clearly communicates the findings!
 ggplot(data=???, aes(x=???, y=???)) +
   geom_boxplot() +
-  ylab("Reaction time (seconds)")
+  geom_jitter(width= 0.1) +
+  ylab("Reaction time (milliseconds)")
